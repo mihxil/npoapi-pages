e7207e73d656969101a9aa3bd5bf7871581ca141
Michiel Meeuwissen <git@meeuw.org>
2021-12-13 12:58:10 +0100

    Added readme and other details.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ docker:
 	docker build -t mihxil/npoapi-pages .
 
 docker-run:
-	docker run --env secret=${secret} -p 5000:5000  mihxil/npoapi-pages
+	docker run --env secret=${secret} --env profile=vpro -p 5000:5000  mihxil/npoapi-pages
 
 run:
-	FLASK_ENV=development configdir=$(pwd) flask run
+	FLASK_ENV=development configdir=$(pwd) profile=vpro flask run
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 import json
 
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 from npoapi import Pages
 from npoapi.data.api import PagesForm, PagesSearchType, TextMatcherListType, TextMatcherType
 
@@ -10,18 +10,20 @@ app = Flask(__name__)
 
 client = Pages(interactive=False,
                debug=os.getenv("debug", default=False)
-               ).configured_login(default_config_dirs=True)
+               ).configured_login(config_dir=os.getenv("configdir"), default_config_dirs=True)
 client.env("prod")
+profile=os.getenv("profile")
 
 @app.route('/')
 def index():
+    prof = request.args.get('profile') or profile
     form = PagesForm()
     form.searches = PagesSearchType()
     form.searches.types = TextMatcherListType()
     matcher = TextMatcherType()
     matcher.value = "HOME"
     form.searches.types.matcher = [matcher]
-    result = client.search(profile = "vpro", form=form)
+    result = client.search(profile = prof, form=form)
     j = json.JSONDecoder().decode(result)
     pages = list(map(lambda i: i['result'], j["items"]))
     return render_template('index.html',  pages=pages)
