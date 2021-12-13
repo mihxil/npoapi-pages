901f6fb451a733f1f6dd0afb0bc7efaa5481a867
Michiel Meeuwissen <git@meeuw.org>
2021-12-13 13:18:27 +0100

    Details.

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import json
 
 from flask import Flask, render_template, send_from_directory, request
 from npoapi import Pages
-from npoapi.data.api import PagesForm, PagesSearchType, TextMatcherListType, TextMatcherType
+from npoapi.data.api import PagesForm, PagesSearchType, TextMatcherListType, TextMatcherType, PageSortType, \
+    PageSortTypeEnum, OrderTypeEnum
 
 app = Flask(__name__)
 
@@ -16,17 +17,24 @@ profile=os.getenv("profile")
 
 @app.route('/')
 def index():
-    prof = request.args.get('profile') or profile
+    profArg = request.args.get('profile')
+    if "" == profArg:
+        prof = None
+    else:
+        prof = profArg  or profile
     form = PagesForm()
+    form.sort_fields = [PageSortType()]
+    form.sort_fields[0].value = PageSortTypeEnum.LAST_MODIFIED
+    form.sort_fields[0].order = OrderTypeEnum.DESC
     form.searches = PagesSearchType()
     form.searches.types = TextMatcherListType()
     matcher = TextMatcherType()
     matcher.value = "HOME"
     form.searches.types.matcher = [matcher]
-    result = client.search(profile = prof, form=form)
+    result = client.search(profile = prof, form=form, limit=240)
     j = json.JSONDecoder().decode(result)
     pages = list(map(lambda i: i['result'], j["items"]))
-    return render_template('index.html',  pages=pages)
+    return render_template('index.html',  pages=pages, result=j, profile=prof)
 
 
 @app.route('/page/<path:url>')
